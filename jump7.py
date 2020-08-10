542f470c8cef73ff1860ac661a2251f40b342dd1
wsm <745087044@qq.com>
2020-08-10 09:42:43 +0800

    new

--- jump7.py
+++ jump7.py
@@ -1,6 +1,11 @@
-for i in range(1,101):
-        if i % 7 == 0 or i % 10 == 7 or i // 10 == 7:
-                    continue
-                    else:
-                                print(i)
+for a in range(1,101):
+    if a % 7 == 0 or a % 10 == 7 or a // 10 == 7:
+        continue
+    else:
+        print(a)
 
+#for a in range(1,101):
+#    if a % 7 == 0 or a % 10 == 7 or a // 10 == 7:
+#        continue
+#    else:
+#        print(a)
